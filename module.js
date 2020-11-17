7aec478ed24940e8743a5e69476451a80b70d130
Timo <Timo@MacBook-Pro-2.local>
2020-11-17 14:38:48 +0100

    debug

--- module.js
+++ module.js
@@ -2,6 +2,7 @@ import path from "path";
 
 export default function nuxtBootstrapVue(moduleOptions) {
   const options = Object.assign({}, this.options.axios, moduleOptions);
+  console.log("module", options);
 
   // Register `plugin.js` template
   this.addPlugin(path.resolve(__dirname, "plugin.js"), { options });
--- plugin.js
+++ plugin.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import StatamicImage from "vue-statamic-image";
 
 let options = JSON.parse(`<%= JSON.stringify(options) %>`);
+console.log("plugin", options);
 
 Vue.use(StatamicImage, {
   ...options,
