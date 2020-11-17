4d2e49870f78f1a1c8d22650d38410e42bb6c61c
Timo <Timo@MacBook-Pro-2.local>
2020-11-17 14:43:29 +0100

    fixes options bug

--- module.js
+++ module.js
@@ -5,7 +5,7 @@ export default function nuxtBootstrapVue(moduleOptions) {
   console.log("module", options);
 
   // Register `plugin.js` template
-  this.addPlugin(path.resolve(__dirname, "plugin.js"), { options });
+  this.addPlugin({ src: path.resolve(__dirname, "plugin.js"), options });
 }
 
 module.exports.meta = require("./package.json");
