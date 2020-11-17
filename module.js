d3b2c172ce8e3dc78e5c32048458af9e524b183e
Timo <Timo@MacBook-Pro-2.local>
2020-11-17 14:21:55 +0100

    fixes module installation bug

--- module.js
+++ module.js
@@ -4,7 +4,7 @@ export default function nuxtBootstrapVue(moduleOptions) {
   const options = Object.assign({}, this.options.axios, moduleOptions);
 
   // Register `plugin.js` template
-  this.addPlugin(path.resolve(__dirname, "plugin.js", { options }));
+  this.addPlugin(path.resolve(__dirname, "plugin.js"), { options });
 }
 
 module.exports.meta = require("./package.json");
